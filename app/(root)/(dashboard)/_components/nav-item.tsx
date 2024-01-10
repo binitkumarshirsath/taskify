144ac494e78b9ffaa85e66880249b11bf4e2dc31
Binit <binitkumar0911@gmail.com>
2024-01-10 23:04:59 +0530

    cont sidebar , add accordion

---
+++ app/(root)/(dashboard)/_components/nav-item.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import {
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Activity, CreditCard, Layout, Settings } from "lucide-react";
+import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
+
+interface Organization {
+  id: string;
+  name: string;
+  slug: string | null;
+  imageUrl: string;
+}
+
+interface NavitemComponentProps {
+  isActive: boolean;
+  organization: Organization;
+  isExpanded: boolean;
+  expandToggle: (id: string) => void;
+}
+
+export const NavitemComponent = ({
+  isActive,
+  organization,
+  expandToggle,
+  isExpanded,
+}: NavitemComponentProps) => {
+  const routes = [
+    {
+      label: "Boards",
+      icon: <Layout className="h-4 w-4 mr-2" />,
+      href: `/organization/${organization.id}`,
+    },
+    {
+      label: "Activity",
+      icon: <Activity className="h-4 w-4 mr-2" />,
+      href: `/organization/${organization.id}/activity`,
+    },
+    {
+      label: "Settings",
+      icon: <Settings className="h-4 w-4 mr-2" />,
+      href: `/organization/${organization.id}/settings`,
+    },
+    {
+      label: "Billing",
+      icon: <CreditCard className="h-4 w-4 mr-2" />,
+      href: `/organization/${organization.id}/billing`,
+    },
+  ];
+
+  const router = useRouter();
+  const pathName = usePathname();
+  return (
+    <AccordionItem value={organization.id} className="border-none">
+      <AccordionTrigger
+        onClick={() => expandToggle(organization.id)}
+        className={cn(
+          "flex items-center gap-x-2 p-1.5 text-neutral-700 rounded-md hover:bg-neutral-500/10 transition text-start no-underline hover:no-underline",
+          isActive && !isExpanded && "bg-sky-500/10 text-sky-700"
+        )}
+      >
+        <div className="flex items-center gap-x-2">
+          <div className="w-7 h-7 relative">
+            <Image
+              fill
+              src={organization.imageUrl}
+              alt="Organization"
+              className="rounded-sm object-cover"
+            />
+          </div>
+          <span className="font-medium text-sm">{organization.name}</span>
+        </div>
+      </AccordionTrigger>
+      <AccordionContent className="pt-1 text-neutral-700">
+        {routes.map((route) => (
+          <Button
+            key={route.href}
+            size={"sm"}
+            onClick={() => router.push(route.href)}
+            className={cn(
+              "w-full font-normal justify-start pl-10 mb-1",
+              pathName === route.href && "bg-sky-500/10 text-sky-700"
+            )}
+            variant="ghost"
+          >
+            {route.icon}
+            {route.label}
+          </Button>
+        ))}
+      </AccordionContent>
+    </AccordionItem>
+  );
+};
--- app/(root)/(dashboard)/_components/sidebar.tsx
+++ app/(root)/(dashboard)/_components/sidebar.tsx
@@ -1,3 +1,99 @@
-export const SideBarComponent = () => {
-  return <div className="">Sidebar!!</div>;
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useOrganization, useOrganizationList } from "@clerk/nextjs";
+import { Plus } from "lucide-react";
+import Link from "next/link";
+import { Accordion } from "@/components/ui/accordion";
+import { NavitemComponent } from "./nav-item";
+import { useLocalStorage } from "usehooks-ts";
+
+interface SideBarComponent {
+  storageKey?: string;
+}
+
+export const SideBarComponent = ({
+  storageKey = "t-sidebar-state",
+}: SideBarComponent) => {
+  // store which are accordions are open
+  const [expanded, setExpanded] = useLocalStorage<Record<string, any>>(
+    storageKey,
+    {}
+  );
+
+  /*
+      { key1 : true,
+        key2 : true,
+        key3 : false,
+        key4 : true
+      }
+      ==>
+      ['key1','key2','key4']
+  */
+
+  // can be passed to accordion to keep it open even after refresh
+  const defaultAccordionValue: string[] = Object.keys(expanded).reduce(
+    (acc: string[], key: string) => {
+      if (expanded[key]) {
+        acc.push(key);
+      }
+      return acc;
+    },
+    []
+  );
+
+  // Open or close the accordion
+  const expandToggle = (id: string) => {
+    return setExpanded((prevData) => ({
+      ...prevData,
+      [id]: !expanded[id],
+    }));
+  };
+
+  const { organization: activeOrganization, isLoaded: isActiveOrgLoaded } =
+    useOrganization();
+
+  const {
+    userMemberships: userJoinedOrganizations,
+    isLoaded: userJoinedOrganizationsLoaded,
+  } = useOrganizationList({
+    userMemberships: {
+      infinite: true,
+    },
+  });
+
+  return (
+    <div className="flex flex-col">
+      {/* Workspaces and + btn */}
+      <div className="flex items-center font-semibold">
+        <div>Workspaces</div>
+        <Button
+          asChild
+          type="button"
+          size={"icon"}
+          variant={"ghost"}
+          className="ml-auto"
+        >
+          <Link href={"/select-org"} className="ml-auto">
+            <Plus height={20} width={20} />
+          </Link>
+        </Button>
+      </div>
+      {/* Accordion */}
+
+      <div className="pt-10 md:pt-8">
+        <Accordion type="multiple" defaultValue={defaultAccordionValue}>
+          {userJoinedOrganizations?.data?.map(({ organization }) => (
+            <NavitemComponent
+              key={organization.id}
+              isActive={activeOrganization?.id === organization.id}
+              organization={organization}
+              isExpanded={expanded[organization.id]}
+              expandToggle={expandToggle}
+            />
+          ))}
+        </Accordion>
+      </div>
+    </div>
+  );
 };
