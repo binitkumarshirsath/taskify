5bca3b40606cb1daee546796fb534d5ae581713c
Binit <binitkumar0911@gmail.com>
2024-01-24 09:55:52 +0530

    add hint and toast

--- app/(root)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(root)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,9 +1,18 @@
+import { Separator } from "@/components/ui/separator";
 import InfoComponent from "./_components/info";
+import { Suspense } from "react";
+import { BoardList } from "./_components/board-list";
 
 const OrganizationPage = () => {
   return (
     <div className="w-full">
       <InfoComponent />
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+        <Suspense fallback={<BoardList.Skeleton />}>
+          <BoardList />
+        </Suspense>
+      </div>
     </div>
   );
 };
